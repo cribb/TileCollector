af3bc28118c7d9077ecdac9d75e575936d3fbc00
Jeremy Cribb <cribb@cs.unc.edu>
2020-10-30 17:13:12 -0400

    New function that plots the mosaic in a figure.

---
+++ tc_show_mosaic.m
@@ -0,0 +1,22 @@
+function tc_show_mosaic(Mosaic)
+
+    im = Mosaic.MosaicTable.Image;
+    imR = reshape(im, Mosaic.MosaicSizeRC);
+    TiledImage = imtile(imR, 'GridSize', Mosaic.MosaicSizeRC);
+
+    %     imXax = linspace(min(Xmat(:)),max(Xmat(:))+imageSize_mm(1),size(T,2));    
+    %     imYax = linspace(min(Ymat(:)),max(Ymat(:))+imageSize_mm(2),size(T,1));
+
+    imXax = [0:size(TiledImage,2)-1] .* Mosaic.LengthScale/1e3;
+    imYax = [0:size(TiledImage,1)-1] .* Mosaic.LengthScale/1e3;
+
+    
+    figure; 
+    imagesc(imXax, imYax, TiledImage);  %#ok<NBRAK>
+    axis image;
+    xlabel('[mm]'); 
+    ylabel('[mm]'); 
+    colormap(gray);
+
+    
+return
