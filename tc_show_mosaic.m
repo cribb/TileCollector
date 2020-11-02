f47d3fbe75ca161706d5cbda157dbf8986e47437
Jeremy Cribb <cribb@cs.unc.edu>
2020-11-02 16:52:35 -0500

    Tweaked input structure configuration.

--- tc_show_mosaic.m
+++ tc_show_mosaic.m
@@ -1,15 +1,14 @@
 function tc_show_mosaic(Mosaic)
 
-    im = Mosaic.MosaicTable.Image;
-    imR = reshape(im, Mosaic.MosaicSizeRC);
-    TiledImage = imtile(imR, 'GridSize', Mosaic.MosaicSizeRC);
+        
+    imR = reshape(Mosaic.ImageTiles, Mosaic.SizeRC);
+    TiledImage = imtile(imR, 'GridSize', Mosaic.SizeRC);
 
     %     imXax = linspace(min(Xmat(:)),max(Xmat(:))+imageSize_mm(1),size(T,2));    
     %     imYax = linspace(min(Ymat(:)),max(Ymat(:))+imageSize_mm(2),size(T,1));
 
     imXax = [0:size(TiledImage,2)-1] .* Mosaic.LengthScale/1e3;
     imYax = [0:size(TiledImage,1)-1] .* Mosaic.LengthScale/1e3;
-
     
     figure; 
     imagesc(imXax, imYax, TiledImage);  %#ok<NBRAK>
